e3c3973cfdc0288370f506191ff164d38d57b34e
Rahim10020 <rahialighi@gmail.com>
2026-04-18 20:17:39 +0000

    delete all clear cl

--- src/app/about/components/FastFactCard.js
+++
@@ -1,8 +0,0 @@
-export default function FastFactCard({ number, label }) {
-  return (
-    <div className="bg-[#1e3a8a] rounded-lg shadow-sm p-4 w-42 h-48 flex flex-col items-center justify-center hover:cursor-pointer">
-      <span className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-[#E1E3E7]">{number}</span>
-      <p className="text-[#E1E3E7] text-sm mt-2 font-medium">{label}</p>
-    </div>
-  );
-}
