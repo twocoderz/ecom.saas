8552172606fd90c97d9c94b274c4c2d6d1256a86
Rahim10020 <rahialighi@gmail.com>
2026-04-18 20:16:58 +0000

    remove folders

--- src/components/sidebar_right/RecentProjectCard.js
+++
@@ -1,8 +0,0 @@
-export default function RecentProjectCard({ title, date }) {
-  return (
-    <div className="border-l-4 border-blue-500 pl-4">
-      <h3 className="text-gray-800 font-bold text-sm md:text-md">{title}</h3>
-      <p className="text-sm text-gray-600">{new Date(date).toLocaleDateString('fr-FR')}</p>
-    </div>
-  );
-}
